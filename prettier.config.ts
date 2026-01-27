bbf7a41ef8103b4bfd4f8ec787011af7745b06b1
AFCMS <afcm.contact@gmail.com>
2026-01-27 11:13:55 +0100

    Add Prettier

---
+++ prettier.config.ts
@@ -0,0 +1,11 @@
+import { type Config } from "prettier";
+
+const config: Config = {
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  singleQuote: false,
+  semi: true,
+};
+
+export default config;
